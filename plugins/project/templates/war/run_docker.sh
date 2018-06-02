09f3e4de9b8dd177bfbed5d6ec24c8a56a9350ef
Daniel Platz <daniel.platz@me.com>
2018-06-02 10:55:52 +0200

    First support for TomEE

--- plugins/project/templates/war/run_docker.sh
+++ plugins/project/templates/war/run_docker.sh
@@ -7,6 +7,7 @@ set -e
 docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/:/opt/jboss/wildfly/standalone/deployments/ jboss/wildfly:10.1.0.Final /opt/jboss/wildfly/bin/standalone.sh --server-config=standalone-full.xml -b "0.0.0.0"
 #docker run --rm --name appsvr -p 80:9080 -p 443:9443 -v $(pwd)/build/libs/:/config/dropins/ websphere-liberty:javaee7
 #docker run --rm --name appsvr -p 9060:9060 -p 80:9080 -p 7777:7777 -v $(pwd)/build/libs:/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/monitoredDeployableApps/servers/server1 38leinad/websphere-9
+#docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.war:/usr/local/tomee/webapps/template-artifactid.war tomee:8-jre-7.0.4-plume
 
 # Java EE 8 servers
 #docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/:/opt/jboss/wildfly/standalone/deployments/ jboss/wildfly:12.0.0.Final /opt/jboss/wildfly/bin/standalone.sh --server-config=standalone-full.xml -b "0.0.0.0" -Dee8.preview.mode=true
