403a3c77e1615af866cb26ccb05db5271187e8de
Daniel Platz <daniel.platz@me.com>
2018-06-10 12:17:19 +0200

    Fixed image-name

--- plugins/project/templates/ear/run_docker.sh
+++ plugins/project/templates/ear/run_docker.sh
@@ -4,4 +4,4 @@ set -e
 ./gradlew build
 docker run --rm --name appsvr -p 80:8080 -v $(pwd)/template-artifactid-ear/build/libs/template-artifactid.ear:/opt/jboss/wildfly/standalone/deployments/template-artifactid.ear jboss/wildfly:10.1.0.Final /opt/jboss/wildfly/bin/standalone.sh --server-config=standalone-full.xml -b "0.0.0.0"
 #docker run --rm --name appsvr -p 80:9080 -p 443:9443 -v $(pwd)/template-artifactid-ear/build/libs/template-artifactid.ear:/config/dropins/template-artifactid.ear websphere-liberty:javaee7
-#docker run --rm --name appsvr -p 9060:9060 -p 80:9080 -p 7777:7777 -v $(pwd)/template-artifactid-ear/build/libs/template-artifactid.ear:/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/monitoredDeployableApps/servers/server1/template-artifactid.ear 38leinad/websphere-9
+#docker run --rm --name appsvr -p 9060:9060 -p 80:9080 -p 7777:7777 -v $(pwd)/template-artifactid-ear/build/libs/template-artifactid.ear:/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/monitoredDeployableApps/servers/server1/template-artifactid.ear 38leinad/was-9
