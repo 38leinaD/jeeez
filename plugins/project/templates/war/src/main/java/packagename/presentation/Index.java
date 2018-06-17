d295028b80886eac609118a16c33c33d147757c4
Daniel Platz <daniel.platz@me.com>
2018-06-17 20:42:40 +0200

    More dynamic output for sample XHTML page

--- plugins/project/templates/war/src/main/java/packagename/presentation/Index.java
+++ plugins/project/templates/war/src/main/java/packagename/presentation/Index.java
@@ -8,17 +8,27 @@ import javax.enterprise.inject.Model;
 public class Index implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	private String input1 = null;
 	private String output = null;
 	
 	public Object clicked() {
 		
 		System.out.println("HELLO!!!");
-		output = "Hello @" + System.currentTimeMillis();
+		output = "Hello " + input1 + " @" + System.currentTimeMillis();
 		return null;
 	}
 	
-	public String output() {
-		return output;
+	public void setInput(String input) {
+		System.out.println("SET TO : " + input);
+		this.input1 = input;
+	}
+
+	public String getInput() {
+		return input1;
+	}
+
+	public String getOutput() {
+		return this.output;
 	}
 }
