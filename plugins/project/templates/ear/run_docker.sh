b0a345a71c559f00247050d677e1fbe07185bbb1
Daniel Platz <daniel.platz@me.com>
2018-09-03 07:46:18 +0200

    Update to Wildfly 14 for JEE 8

--- plugins/project/templates/ear/run_docker.sh
+++ plugins/project/templates/ear/run_docker.sh
@@ -11,6 +11,6 @@ docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifacti
 
 
 # Java EE 8 servers
-#docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.ear:/opt/jboss/wildfly/standalone/deployments/template-artifactid.war jboss/wildfly:13.0.0.Final /opt/jboss/wildfly/bin/standalone.sh --server-config=standalone-full.xml -b "0.0.0.0" -Dee8.preview.mode=true
+#docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.ear:/opt/jboss/wildfly/standalone/deployments/template-artifactid.war jboss/wildfly:14.0.0.Final /opt/jboss/wildfly/bin/standalone.sh --server-config=standalone-full.xml -b "0.0.0.0" -Dee8.preview.mode=true
 #docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.ear:/glassfish5/glassfish/domains/domain1/autodeploy/template-artifactid.war oracle/glassfish:5.0
 #docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.ear:/opt/payara5/glassfish/domains/domain1/autodeploy/template-artifactid.war payara/server-full:5-SNAPSHOT
