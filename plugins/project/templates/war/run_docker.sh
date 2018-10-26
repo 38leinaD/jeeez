7700af3ad8ef08a5506f20828d43d6e7d6c53cb0
Daniel Platz <daniel.platz@me.com>
2018-10-27 00:36:50 +0200

    Add wlp javaee8 docker

--- plugins/project/templates/war/run_docker.sh
+++ plugins/project/templates/war/run_docker.sh
@@ -17,3 +17,4 @@ docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifacti
 #docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.war:/opt/jboss/wildfly/standalone/deployments/template-artifactid.war jboss/wildfly:14.0.0.Final /opt/jboss/wildfly/bin/standalone.sh --server-config=standalone-full.xml -b "0.0.0.0" -Dee8.preview.mode=true
 #docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.war:/glassfish5/glassfish/domains/domain1/autodeploy/template-artifactid.war oracle/glassfish:5.0
 #docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.war:/opt/payara5/glassfish/domains/domain1/autodeploy/template-artifactid.war payara/server-full:5-SNAPSHOT
+#docker run --rm --name appsvr -p 80:9080 -p 443:9443 -v $(pwd)/build/libs/template-artifactid.war:/config/dropins/template-artifactid.war openliberty/open-liberty:javaee8
