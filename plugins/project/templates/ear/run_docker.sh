d1fbed4f61b7a66c52fc236ce5cead2f2a7a33bf
Daniel Platz <daniel.platz@me.com>
2020-02-02 19:39:38 +0100

    Update to latest Websphere version/docker images

--- plugins/project/templates/ear/run_docker.sh
+++ plugins/project/templates/ear/run_docker.sh
@@ -6,7 +6,7 @@ set -e
 # Java EE 7 servers
 docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.ear:/opt/jboss/wildfly/standalone/deployments/template-artifactid.ear jboss/wildfly:10.1.0.Final /opt/jboss/wildfly/bin/standalone.sh --server-config=standalone-full.xml -b "0.0.0.0"
 #docker run --rm --name appsvr -p 80:9080 -p 443:9443 -v $(pwd)/build/libs/template-artifactid.ear:/config/dropins/template-artifactid.ear websphere-liberty:javaee7
-#docker run --rm --name appsvr -p 9060:9060 -p 80:9080 -p 7777:7777 -v $(pwd)/build/libs/template-artifactid.ear:/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/monitoredDeployableApps/servers/server1/template-artifactid.ear 38leinad/was-9
+#docker run --rm --name appsvr -p 9060:9060 -p 80:9080 -p 7777:7777 -v $(pwd)/build/libs/template-artifactid.ear:/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/monitoredDeployableApps/servers/server1/template-artifactid.ear 38leinad/was:latest
 #docker run --rm --name appsvr -p 80:8080 -v $(pwd)/build/libs/template-artifactid.ear:/usr/local/tomee/webapps/template-artifactid.ear tomee:8-jre-7.1.0-plus
 
 
